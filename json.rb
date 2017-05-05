b2f13f30fd7254dcc70507bf06aa62295a1b47bb
Douglas Beal <git@dougbeal.com>
2017-05-04 19:19:11 -0700

    save each board into seperate file in output directory

--- json.rb
+++ json.rb
@@ -2,24 +2,29 @@ require 'trello'
 load 'foolscap-secret/trello.rb'
 require 'pp'
 require 'ap'
+require 'pathname'
 
 #me = Trello::Member.find("me")
+path = Pathname.new('output')
+path.mkdir if not path.exist?
 
 # 54f7d5e278b19c12650c06a3  - Convention Tasks
 boardid = "54f7d5e278b19c12650c06a3"
 board = Trello::Board.find(boardid)
 board.lists.each do |list|
   if list.name =~ /[mM]eeting/
-    puts "# #{list.id} - #{list.name}"
-    list.cards( :members => true ).each do |card|
-      member_string = card.members.map {|member| member.username }.join(",")
-      puts "## #{card.name} [#{member_string}]"
-      puts "#{card.desc}" if card.desc
-      card.actions(:filter => "commentCard").each do |action|
-        puts "* #{action.data["text"]}"
+    filename = "#{list.id} - #{list.name}"
+    File.open(path.join(filename), 'w') { |f|
+      f.puts "# #{list.id} - #{list.name}"
+      list.cards( :members => true ).each do |card|
+        member_string = card.members.map {|member| member.username }.join(",")
+        f.puts "## #{card.name} [#{member_string}]"
+        f.puts "#{card.desc}" if card.desc
+        card.actions(:filter => "commentCard").each do |action|
+          f.puts "* #{action.data["text"]}"
+        end
       end
-    end
-
+    }
   end
 end
 
